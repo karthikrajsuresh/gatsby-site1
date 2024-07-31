ad5136471e6972ca9d4a67fa90c5b1e93118b116
Karthik Raj S <karthikrajsuresh007@gmail.com>
2024-07-31 16:59:30 +0530

    Finished Gatsby Tutorial Part 3

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    //siteUrl: `https://www.yourdomain.tld`,
+    title: "gatsby-site1",
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
+}
